0d65c0839ebeed17ce112a78af297a25bd32c558
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2022-06-07 15:49:15 +0500

    initial commit

---
+++ market/__init__.py
@@ -0,0 +1,20 @@
+"""
+Сервис с REST API, анализирующий цены различных Товаров
+Вступительное испытание в ШБР Яндекса лета 2022
+"""
+
+__author__ = 'Alexander Vinokurov'
+__maintainer__ = __author__
+
+__email__ = 'alexander-vin0@yandex.ru'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
