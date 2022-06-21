fb4395183daeea3964759ab7bdbdf8d3ea491b04
AlexandrVino <ubuntu@have-1826.lxd>
2022-06-21 11:57:04 +0000

    edit unit_tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -8,7 +8,7 @@ import urllib.error
 import urllib.parse
 import urllib.request
 
-API_BASEURL = "http://localhost:8081"
+API_BASEURL = "http://localhost:80"
 
 ROOT_ID = "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1"
 
